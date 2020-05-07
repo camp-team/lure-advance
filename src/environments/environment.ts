d8da278a81b9454121002191ca7caa38e46b45d9
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-07 17:53:24 +0900

    feat(firebase):env設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC3LvvHxUQXVWMhIQcQAueSZ7XjdJg4jvM',
+    authDomain: 'lure-advance-1296d.firebaseapp.com',
+    databaseURL: 'https://lure-advance-1296d.firebaseio.com',
+    projectId: 'lure-advance-1296d',
+    storageBucket: 'lure-advance-1296d.appspot.com',
+    messagingSenderId: '674251775528',
+    appId: '1:674251775528:web:c4340e85c4996dd3331f1d',
+    measurementId: 'G-SSQQ7X37EM',
+  },
 };
 
 /*
