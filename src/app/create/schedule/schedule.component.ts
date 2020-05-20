81d7f7186be265b3a9bd090bd6a16f6391f1f219
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-20 16:58:30 +0900

    feat(schedule):タイマー追加

--- src/app/create/schedule/schedule.component.ts
+++ src/app/create/schedule/schedule.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-schedule',
@@ -6,15 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.scss'],
 })
 export class ScheduleComponent implements OnInit {
-  startDate: Date = new Date();
-  times = new Array(60).map((_, i) => {
-    i + 15;
+  @Input() form: FormGroup;
+  seletedDate: FormControl = new FormControl(moment().add(1, 'days'));
+  toDay: Date = new Date();
+  startDate: Date = moment(this.toDay).add(1, 'days').toDate();
+  untilDate: Date = moment(this.toDay).add(6, 'months').toDate();
+
+  currentYear: number = this.toDay.getFullYear();
+  currentDate: number = this.toDay.getDate();
+  currentMonth: number = this.toDay.getMonth();
+
+  times: Date[] = new Array(96).fill(null).map((_, index) => {
+    const currentYear: number = this.toDay.getFullYear();
+    const currentDate: number = this.toDay.getDate();
+    const currentMonth: number = this.toDay.getMonth();
+    const m = (index % 4) * 15;
+    const h = (index * 15) / 60;
+    return new Date(currentYear, currentMonth, currentDate, h, m);
   });
+  selectedTime: Date = this.times[0];
+  selectedSetting: boolean = true;
 
-  constructor() {}
+  clear() {}
+
+  constructor(private fb: FormBuilder) {}
   ngOnInit(): void {}
-}
-
-interface Time {
-  value: string;
 }
