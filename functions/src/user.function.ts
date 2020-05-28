0a8436a0dfc3c2b4fe460d50b1d27038c98686c1
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-28 22:08:12 +0900

    fix(user.function):createUserにuid追加

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -4,13 +4,15 @@ import { shouldEventRun, markEventTried } from './util';
 
 const db = admin.firestore();
 
-export const createUser = functions.auth
-  .user()
+export const createUser = functions
+  .region('asia-northeast1')
+  .auth.user()
   .onCreate(async (user, context) => {
     const eventId = context.eventId;
     const should = await shouldEventRun(eventId);
     if (should) {
       await db.doc(`users/${user.uid}`).set({
+        uid: user.uid,
         email: user.email,
         avatarURL: user.photoURL,
         name: user.displayName,
