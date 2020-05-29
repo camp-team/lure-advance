4755b41b8fa07c1fd4a1239dcd8e9271f6128424
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-29 17:21:28 +0900

    feat(home):いいねボタン追加

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ThingService } from 'src/app/services/thing.service';
-import { Observable } from 'rxjs';
 import { Thing } from 'src/app/interfaces/thing';
 import { User } from 'src/app/interfaces/user';
 import { UserService } from 'src/app/services/user.service';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,32 +11,59 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  things$: Observable<Thing[]> = this.thingService.getAllThings();
   user: User;
+  things: Thing[];
   likedThingsIds: string[];
+  likeState = {};
 
   constructor(
     private thingService: ThingService,
     private userService: UserService
   ) {
     this.userService.user$.subscribe((user) => {
-      if (user) {
-        this.user = user;
-        this.thingService
-          .getlikedThingIds(this.user.uid)
-          .then((res) => (this.likedThingsIds = res));
-      }
+      this.user = user;
+      this.thingService
+        .getlikedThingIds(this.user.uid)
+        .then((res) => (this.likedThingsIds = res));
     });
+
+    //TODO アルゴリア連携
+    this.thingService
+      .getAllThings()
+      .pipe(
+        map((res) => res.map((item) => item)),
+        take(1)
+      )
+      .toPromise()
+      .then((res) => (this.things = res));
   }
 
   isMore: boolean;
 
-  likeThing(thing: Thing): void {
-    this.thingService.likeThing(thing.id, this.user.uid);
+  likeThing(thingId: string): Promise<void> {
+    const index = this.things.findIndex((thing) => thing.id === thingId);
+    this.things[index].likeCount++;
+    this.likeState[thingId] = {
+      isLike: true,
+    };
+    return this.thingService.likeThing(thingId, this.user.uid);
+  }
+
+  unLikeThing(thingId: string): Promise<void> {
+    const index = this.things.findIndex((thing) => thing.id === thingId);
+    this.things[index].likeCount--;
+    this.likeState[thingId] = {
+      isLike: false,
+    };
+    return this.thingService.unLikeThing(thingId, this.user.uid);
   }
 
   isLike(thingId: string): boolean {
-    return this.likedThingsIds?.includes(thingId);
+    if (this.likeState[thingId]?.isLike) {
+      return this.likeState[thingId].isLike;
+    } else {
+      return this.likedThingsIds?.includes(thingId);
+    }
   }
 
   isUnlike(thingId: string): boolean {
