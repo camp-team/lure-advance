50c4b97bb5351dbc17a6b897d17890493bfd248c
ReiMeroy <xvwlllwvx@gmail.com>
2020-06-04 14:13:51 +0900

    feat(pipe):通知メッセージ

---
+++ src/app/pipe/notifymsg.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NotifymsgPipe } from './notifymsg.pipe';
+
+describe('NotifymsgPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NotifymsgPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
