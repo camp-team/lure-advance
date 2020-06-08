a72dad6cd202af8c04906c3187d3f9f6b37a7b40
ReiMeroy <xvwlllwvx@gmail.com>
2020-06-08 14:52:44 +0900

    refactor(user.service,thing.service)

--- src/app/services/thing.service.ts
+++ src/app/services/thing.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable, combineLatest } from 'rxjs';
+import { Observable, combineLatest, of } from 'rxjs';
 import { Thing, ThingWithUser } from '../interfaces/thing';
 import { firestore } from 'firebase';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { map, take, switchMap } from 'rxjs/operators';
+import { map, take, switchMap, takeLast } from 'rxjs/operators';
 import { User } from '../interfaces/user';
 import { UserService } from './user.service';
-import { Notification } from '../interfaces/notification';
 
 @Injectable({
   providedIn: 'root',
@@ -20,22 +19,23 @@ export class ThingService {
   ) {}
 
   getThings(): Observable<ThingWithUser[]> {
-    let allthings: Thing[];
     return this.db
       .collection<Thing>(`things`, (ref) => ref.orderBy('updateAt', 'desc'))
       .valueChanges()
       .pipe(
         switchMap((things: Thing[]) => {
-          allthings = things;
-          const distinctUids: string[] = [
-            ...new Set(things.map((thing) => thing.designerId)),
-          ];
-          return combineLatest(
+          const distinctUids: string[] = Array.from(
+            new Set(things.map((thing) => thing.designerId))
+          );
+
+          const users$: Observable<User[]> = combineLatest(
             distinctUids.map((uid) => this.userService.getUserByID(uid))
           );
+
+          return combineLatest([of(things), users$]);
         }),
-        map((users: User[]) => {
-          return allthings.map((thing) => {
+        map(([things, users]) => {
+          return things.map((thing) => {
             return {
               ...thing,
               user: users.find((user) => thing.designerId === user.uid),
@@ -43,6 +43,10 @@ export class ThingService {
           });
         })
       );
+  }
+
+  getThingsWithPromise(): Promise<ThingWithUser[]> {
+    return this.getThings().pipe(take(1)).toPromise();
   }
 
   getThingByID(thingId: string): Observable<Thing> {
@@ -76,28 +80,39 @@ export class ThingService {
     );
   }
 
-  likeThing(thing: Thing, user: User): Promise<void> {
-    return this.db.doc(`things/${thing.id}/likeUsers/${user.uid}`).set({
+  likeThing(thing: Thing, uid: string): Promise<void> {
+    return this.db.doc(`things/${thing.id}/likeUsers/${uid}`).set({
+      thingId: thing.id,
       designerId: thing.designerId,
-      fromUid: user.uid,
+      fromUid: uid,
     });
   }
 
   unLikeThing(thingId: string, uid: string): Promise<void> {
+    console.log('hogehoge');
     return this.db.doc(`things/${thingId}/likeUsers/${uid}`).delete();
   }
 
-  getlikedThingIds(uid: string): Promise<string[]> {
+  getLikedThingIds(uid: string): Promise<string[]> {
     return this.db
       .collectionGroup<{
-        uid: string;
         thingId: string;
-      }>('likeUser', (ref) => ref.where('uid', '==', uid))
+        fromUid: string;
+        designerId: string;
+      }>('likeUsers', (ref) => ref.where('fromUid', '==', uid))
       .valueChanges()
       .pipe(
         map((res) => res.map((item) => item.thingId)),
         take(1)
       )
+      .toPromise();
+  }
+
+  getMyLikedThingsByUid(uid: string): Promise<string[]> {
+    return this.db
+      .collection<string>(`users/${uid}/likedThings`)
+      .valueChanges()
+      .pipe(take(1))
       .toPromise();
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
-import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +11,5 @@ export class UserService {
 
   getUserByID(uid: string): Observable<User> {
     return this.db.doc<User>(`users/${uid}`).valueChanges();
-  }
-
-  getUserByIDonPromise(uid: string): Promise<User> {
-    return this.db
-      .doc<User>(`users/${uid}`)
-      .valueChanges()
-      .pipe(take(1))
-      .toPromise();
   }
 }
