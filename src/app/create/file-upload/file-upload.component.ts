d2a8ef9671a2c6a84882a34087fd5119d0bfa018
ReiMeroy <xvwlllwvx@gmail.com>
2020-06-22 18:17:09 +0900

    feat(create.component):ファイルアップロード追加

--- src/app/create/file-upload/file-upload.component.ts
+++ src/app/create/file-upload/file-upload.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AngularFireUploadTask } from '@angular/fire/storage/task';
 import { ThingService } from 'src/app/services/thing.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-file-upload',
@@ -10,18 +10,62 @@ import { ThingService } from 'src/app/services/thing.service';
 export class FileUploadComponent implements OnInit {
   @Input() thingId: string;
 
-  constructor(private thingService: ThingService) {}
-
-  jobs: AngularFireUploadTask[] = [];
-  files: File[];
+  MAX_FILE_LENGTH = 5;
+  constructor(
+    private thingService: ThingService,
+    private snackBar: MatSnackBar
+  ) {}
   ngOnInit(): void {}
 
+  images: any[] = [];
+  imageFiles: (File | string)[] = [];
+
+  stls: (string | ArrayBuffer)[] = [];
+  stlFiles: (File | string)[] = [];
+
   selectFiles(event) {
-    //TODO クロッピング
-    this.files = Object.values(event.target.files);
+    const files: File[] = Object.values(event.target.files);
+    if (files.length + this.images.length > this.MAX_FILE_LENGTH) {
+      this.snackBar.open('最大ファイル数は5つです');
+      return;
+    }
+    files.forEach((file) => this.readFile(file));
   }
 
-  uploadFiles() {
-    this.thingService.uploadThings(this.thingId, this.files);
+  private readFile(file: File) {
+    const fr: FileReader = new FileReader();
+    fr.onload = (e) => {
+      if (this.isStl(file)) {
+        this.stlFiles.push(file);
+        this.stls.push(e.target.result);
+      } else {
+        this.images.push(e.target.result);
+        this.imageFiles.push(file);
+      }
+    };
+    fr.readAsDataURL(file);
+  }
+
+  private isStl(file: File) {
+    const fileName = file.name.toLocaleLowerCase();
+    return fileName.endsWith('.stl');
+  }
+
+  deleteImage(index: number) {
+    this.images.splice(index, 1);
+    this.imageFiles.splice(index, 1);
+  }
+
+  deleteStl(index: number) {
+    this.stls.splice(index, 1);
+    this.stlFiles.splice(index, 1);
+  }
+
+  async uploadFiles() {
+    return await this.thingService.uploadFiles(
+      this.thingId,
+      this.stlFiles,
+      this.imageFiles
+    );
   }
 }
