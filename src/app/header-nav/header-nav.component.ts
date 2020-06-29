64684246f0e7eb59ca6082c33d61fd0b3f3e7bfe
ReiMeroy <xvwlllwvx@gmail.com>
2020-06-30 02:13:37 +0900

    feat(header-nav.component):autocompleteと検索機能

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -4,6 +4,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from '@interfaces/user';
 import { Observable } from 'rxjs';
 import { NotificationService } from '../services/notification.service';
+import { FormControl } from '@angular/forms';
+import { SearchService } from '../services/search.service';
+import { startWith } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-nav',
@@ -11,38 +15,58 @@ import { NotificationService } from '../services/notification.service';
   styleUrls: ['./header-nav.component.scss'],
 })
 export class HeaderNavComponent implements OnInit {
+  private index = this.searchService.index.things;
+  searchOptions = [];
+
+  user$: Observable<User> = this.authService.user$;
+
+  isProccesing: boolean;
+
+  ctrl: FormControl = new FormControl('');
   constructor(
     private authService: AuthService,
     private snackBar: MatSnackBar,
-    private notificationService: NotificationService
-  ) {}
+    private notificationService: NotificationService,
+    private searchService: SearchService,
+    private router: Router
+  ) {
+    this.ctrl.valueChanges.pipe(startWith('')).subscribe((key) => {
+      this.index.search('').then((result) => {
+        this.searchOptions = result.hits;
+      });
+    });
+  }
 
   @Output() activity: EventEmitter<boolean> = new EventEmitter();
 
   ngOnInit(): void {}
 
-  user$: Observable<User> = this.authService.user$;
-  uid: String = 'xxxx';
-  // user: User;
-  isProccesing: boolean;
-
   login() {
     this.isProccesing = true;
     this.authService
       .login()
-      .then(() => {
-        this.snackBar.open('ログインしました', null, {
-          duration: 2000,
-        });
-      })
-      .finally(() => {
-        this.isProccesing = false;
-      });
+      .then(() => this.snackBar.open('ログインしました'))
+      .finally(() => (this.isProccesing = false));
   }
 
   logout() {
-    this.authService.logout().then(() => {
-      this.snackBar.open('ログアウトしました', null, { duration: 2000 });
+    this.authService
+      .logout()
+      .then(() => this.snackBar.open('ログアウトしました'));
+  }
+
+  routeSearch(searchQuery) {
+    this.router.navigate([''], {
+      queryParams: {
+        searchQuery: searchQuery || null,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  setSearchQuery(seachQuery) {
+    this.ctrl.setValue(seachQuery, {
+      emitEvent: false,
     });
   }
 
