75fa23e8c252b944d570608922fa40544385a33a
ReiMeroy <xvwlllwvx@gmail.com>
2020-07-03 17:33:53 +0900

    feat(home.componet):divider追加

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,30 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { SharedModule } from '../shared/shared.module';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { MatDialogModule } from '@angular/material/dialog';
+import { SharedModule } from '../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
     MatMenuModule,
     SharedModule,
+    MatDividerModule,
     MatProgressSpinnerModule,
     InfiniteScrollModule,
-    MatDialogModule,
   ],
 })
 export class HomeModule {}
