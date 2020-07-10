2f2ce262363c127efd04403fb3a9e4a812dc46cc
ReiMeroy <xvwlllwvx@gmail.com>
2020-07-10 14:03:17 +0900

    feat(activity.component):サイドnavのサービス化&不要なng-template削除

---
+++ src/app/services/ui/drawer.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DrawerService {
+  isOpenSource = new ReplaySubject<boolean>(1);
+  isOpen$: Observable<boolean> = this.isOpenSource.asObservable();
+  isOpened: boolean;
+
+  constructor() {}
+  toggle() {
+    this.isOpened = !this.isOpened; // 反転させる
+    this.isOpenSource.next(this.isOpened);
+  }
+}
